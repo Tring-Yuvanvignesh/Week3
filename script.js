58c57401b7ab136174d4b05cea95b72212e81730
Tring-yuvanvignesh <yuvanvignesh.r@tringapps.com>
2025-02-18 12:58:03 +0530

    Commiting changes

--- script.js
+++ script.js
@@ -3,34 +3,17 @@ document.addEventListener("DOMContentLoaded", function () {
     const signUpButton = document.querySelector(".switch-buttons button:nth-child(2)");
 
     signInButton.addEventListener("click", function () {
-        toggleActive(signInButton, signUpButton);
-        window.location.href = "signIn.html"; // Redirect to sign-in page
+        window.location.href = "signIn.html"; 
     });
 
     signUpButton.addEventListener("click", function () {
-        toggleActive(signUpButton, signInButton);
-        window.location.href = "signUp.html"; // Redirect to sign-up page
+        window.location.href = "signUp.html";
     });
 
-    function toggleActive(activeButton, inactiveButton) {
-        activeButton.classList.add("active");
-        activeButton.classList.remove("inactive");
-
-        inactiveButton.classList.add("inactive");
-        inactiveButton.classList.remove("active");
-    }
 });
 
 function signIn(){
         window.location.href = "signIn.html";
-}
-
-function showTab(tabName) {
-    document.querySelectorAll('.tab').forEach(tab => tab.classList.remove('active'));
-    document.querySelector(`.tab[onclick="showTab('${tabName}')"]`).classList.add('active');
-
-    document.getElementById('industries-content').style.display = tabName === 'industries' ? 'block' : 'none';
-    document.getElementById('services-content').style.display = tabName === 'services' ? 'block' : 'none';
 }
 
 
