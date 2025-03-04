42b294017f8546a63e8c4144e1374042b2a7e452
Tring-yuvanvignesh <yuvanvignesh.r@tringapps.com>
2025-03-04 18:00:44 +0530

    commiting changes

--- script.js
+++ script.js
@@ -65,6 +65,20 @@ function signIn() {
     location.href= "signIn.html"
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+    const teamMenu = document.querySelector(".team-menu");
+    const buttons = teamMenu.querySelectorAll("button");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", function () {
+            buttons.forEach(btn => btn.classList.remove("active"));
+
+            this.classList.add("active");
+        });
+    });
+});
+
+
 
 
 
